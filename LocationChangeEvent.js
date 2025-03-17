c401175eff0ca6f0e95cd905ef8c6df70f08c697
Charles Kim <52868068+charleskimbac@users.noreply.github.com>
2025-03-16 22:36:35 -0400

    add export

--- LocationChangeEvent.js
+++ LocationChangeEvent.js
@@ -1,28 +1,35 @@
-/*  v2025.2.9 from https://github.com/charleskimbac/common-files
-    whenever url changes, send "locationchange" event.
-    
-    add `"world": "MAIN"` in the content_script key in manifest.json. this is to access the "history" world object.
+/*  v2025.3.16 from https://github.com/charleskimbac/js-common-files
+    wait for an element to appear in the DOM, or return it if already present. 
+
+    selector: query selector string
+    timeoutDuration: time (ms) until function will timeout and return null
+    parent: Node to watch (default: document.body)
 */
-
-(() => {
-    const oldPushState = history.pushState;
-    history.pushState = function(...args) {
-        const old = oldPushState.apply(this, args);
-        window.dispatchEvent(new Event("locationchange"));
-        return old;
-    };
-
-    const oldReplaceState = history.replaceState;
-    history.replaceState = function(...args) {
-        const old = oldReplaceState.apply(this, args);
-        window.dispatchEvent(new Event("locationchange"));
-        return old;
-    };
-
-    if (!window._locationChangeListenerAttached) {
-        window.addEventListener("popstate", () => {
-            window.dispatchEvent(new Event("locationchange"));
-        });
-        window._locationChangeListenerAttached = true;
+async function waitForElement(selector, timeoutDuration, parent = document.body) {
+    const element = parent.querySelector(selector);
+    if (element) {
+        return element;
     }
-})();
+
+    return new Promise((resolve) => {
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach(() => {
+                const element = parent.querySelector(selector);
+                if (element) {
+                    observer.disconnect();
+                    resolve(element);
+                }
+            });
+        });
+        observer.observe(parent, {childList: true, subtree: true});
+
+        if (timeoutDuration) {
+            setTimeout(() => {
+                observer.disconnect();
+                resolve(null);
+            }, timeoutDuration);
+        }
+    });
+}
+
+export default waitForElement;
